cf9afaad14e5f3b5b9f5cfc994efbe7e0df3deda
Sea for PC <18912387311@163.com>
2019-04-01 19:42:12 +0800

    弱类型事件

---
+++ SeaSharp.Utils/CSharp/WeakEventHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaSharp.Utils.CSharp
+{
+    using System;
+    using System.Reflection;
+    namespace Artech.MemLeakByEvents
+    {
+        public class WeakEventHandler<TEventArgs> where TEventArgs : EventArgs
+        {
+            public WeakReference Reference { get; private set; }
+
+            public MethodInfo Method { get; private set; }
+
+            public EventHandler<TEventArgs> Handler { get; private set; }
+
+            public WeakEventHandler(EventHandler<TEventArgs> eventHandler)
+            {
+                Reference = new WeakReference(eventHandler.Target);
+                Method = eventHandler.Method;
+                Handler = Invoke;
+            }
+
+            public void Invoke(object sender, TEventArgs e)
+            {
+                object target = Reference.Target;
+                if (null != target)
+                {
+                    Method.Invoke(target, new object[] { sender, e });
+                }
+            }
+
+            public static implicit operator EventHandler<TEventArgs>(WeakEventHandler<TEventArgs> weakHandler)
+            {
+                return weakHandler.Handler;
+            }
+        }
+    }
+}
